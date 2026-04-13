9486d06bbe2f94244c91550c54e27bcc312823c9
Miguel Mendoza <miguelmendoza@vizz>
2026-04-13 12:34:09 +0200

    feat(iso-docs): add IsoDocNoteDB model

--- backend/app/modules/iso_docs/models/__init__.py
+++ backend/app/modules/iso_docs/models/__init__.py
@@ -4,3 +4,4 @@ from app.modules.iso_docs.models.metadata import IsoDocMetadataDB
 from app.modules.iso_docs.models.registry_type import RegistryTypeDB
 from app.modules.iso_docs.models.registry_row import RegistryRowDB
 from app.modules.iso_docs.models.registry_attachment import RegistryAttachmentDB
+from app.modules.iso_docs.models.note import IsoDocNoteDB
